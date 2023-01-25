8cf404de54917ba0c73f4bb7fe1636a49f437d72
kjpar0317 <kjpar0317@gmail.com>
2023-01-26 00:25:59 +0900

    설정 수정

--- api-inventory/src/main/java/com/orchestration/inventory/InventoryApplication.java
+++ api-inventory/src/main/java/com/orchestration/inventory/InventoryApplication.java
@@ -2,7 +2,9 @@ package com.orchestration.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class InventoryApplication {
 	public static void main(String[] args) {
